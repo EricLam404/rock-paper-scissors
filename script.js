26b7a79ad3f0f196374a90b50a9e1c3e2da2fb28
EricLam404 <lameric9888@gmail.com>
2022-07-15 21:07:08 -0400

    revised rock paper scissors

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var playerScore = 0;
+var computerScore = 0;
+var scoreboard = document.createElement('div');
+const scoreContainer = document.querySelector("h1");
+
+scoreboard.textContent = "Player: " + playerScore + " vs Computer: " + computerScore;
+scoreContainer.appendChild(scoreboard)
+
 let computerPlay = () =>{
     const choice = ["rock", "paper", "scissor"];
 
@@ -11,28 +19,34 @@ let playRound = (playerSelection, computerSelection) => {
             return "You tied! Rock ties with Rock" 
         }
         if(computerSelection == "paper"){
+            computerScore++;
             return "You lost! Paper beats Rock" 
         }
         if(computerSelection == "scissor"){
+            playerScore++;
             return "You won! Rock beats Scissor" 
         }     
     }
     if(playerSelection == "paper"){
         if(computerSelection == "rock"){
+            playerScore++;
             return "You won! Paper beats Rock" 
         }
         if(computerSelection == "paper"){
             return "You tied! Paper ties with paper" 
         }
         if(computerSelection == "scissor"){
+            computerScore++;
             return "You lost! Scissor beats Paper" 
         }     
     }
     if(playerSelection == "scissor"){
         if(computerSelection == "rock"){
+            computerScore++;
             return "You lost! Rock beats Scissor" 
         }
         if(computerSelection == "paper"){
+            playerScore++;
             return "You won! Scissor beats Paper" 
         }
         if(computerSelection == "scissor"){
@@ -42,9 +56,44 @@ let playRound = (playerSelection, computerSelection) => {
     return "That is not a valid choice!"
 }
 
+const buttons = document.querySelectorAll("button");
+var count = 0;
+
+buttons.forEach(button => button.addEventListener('click', playerSelection))
+
+function playerSelection(e) {
+    const container = document.querySelector(".container")
+    if(count < 5){
+        count++;
+        let playerSelection = this.className;
+        const computerSelection = computerPlay();
+        const winner = playRound(playerSelection, computerSelection);
+        scoreboard.textContent = "Player: " + playerScore + " vs Computer: " + computerScore;
+        const div = document.createElement('div');
+        div.textContent = winner;
+        const vs = document.createElement('div');
+        vs.textContent = playerSelection + " vs " + computerSelection;
+        container.appendChild(vs);
+        container.appendChild(div);
+    } else if(count <= 5){
+        count++
+        const win = document.createElement('div');
+        var winner = "";
+        if(playerScore > computerScore)
+            winner = "You won!"
+        else
+            winner = "The computer won"
+        win.textContent = winner;
+        container.appendChild(win);
+    }
+    
+}
+
+/*
 for(let i = 0; i < 5; i++){
     let playerSelection = prompt("Rock Paper or Scissors?")
     const computerSelection = computerPlay();
     console.log(playerSelection + " vs " + computerSelection);
     console.log(playRound(playerSelection, computerSelection));
 }
+*/
